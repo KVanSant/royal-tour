a8a6789716c302443455781ad5ed2fef2c7e5472
Kristopher Van Sant <kristopher@signaltransmit.com>
2016-03-02 17:14:36 -0800

    finished navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,23 +2,5 @@
 /* Dropdown mobile menu - When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropMenu() {
-  document.getElementById("mobile-dropdown").classList.toggle("show");
-}
-
-
-
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.drop-button')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-list");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+  document.getElementById("slide-menu").classList.toggle("show");
 }
